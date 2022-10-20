84bd68401d2b3e8dfa652d012cf6754d1c39ebe4
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-10-20 16:10:53 +0800

    chore(pkg.microsoft.graph): consist API return types

--- pkg/microsoft/graph.go
+++ pkg/microsoft/graph.go
@@ -2,6 +2,7 @@ package microsoft
 
 import (
 	"fmt"
+
 	"github.com/guonaihong/gout"
 	"github.com/tidwall/gjson"
 )
@@ -31,11 +32,11 @@ func GetUserInfo(id, secret, refresh string) (string, string, error) {
 	return "", "", fmt.Errorf(content)
 }
 
-func GetOutlookMails(id, secret, refresh string) (string, error) {
+func GetOutlookMails(id, secret, refresh string) (string, string, error) {
 	var content string
 	newRefresh, access, err := GetToken(id, secret, refresh)
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
 
 	err = gout.GET(graphURL + "/v1.0/me/messages").
@@ -45,11 +46,11 @@ func GetOutlookMails(id, secret, refresh string) (string, error) {
 		BindBody(&content).
 		Do()
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
 	// 这里的.需要转义，否则会按路径的方式解析
 	if gjson.Get(content, "@odata\\.context").String() != "" {
-		return newRefresh, nil
+		return newRefresh, content, nil
 	}
-	return "", fmt.Errorf(gjson.Get(content, "error").String())
+	return "", "", fmt.Errorf(gjson.Get(content, "error").String())
 }
